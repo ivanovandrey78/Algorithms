a35ed7bcc54fafdf19d7feba8d232efcdae6a779
ivanovandrey78 <jaydenpull1@gmail.com>
2025-10-20 13:18:16 +0300

    Add complex header file

---
+++ algorithms/math/complex.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <iostream>
+
+namespace mymath {
+    
+class Complex {
+private:
+
+    double real;
+    double imag;
+    
+public:
+    // Construct. 
+    
+    Complex();
+
+    Complex(double r);
+    Complex(double i);
+
+    Complex(double r, double i);
+
+    Complex(const Complex &other); // copy other number 
+    
+    // Get-operators
+    double getReal() const;
+    double getImag() const;
+
+    // Edit-operators 
+    void setReal(double r);
+    void setImag(double i);
+    
+    // Arithmetics functions 
+    Complex operator+(const Complex& other) const;
+    Complex operator-(const Complex& other) const;
+    Complex operator*(const Complex& other) const;
+    Complex operator/(const Complex& other) const;
+    
+    // Compare
+    bool operator==(const Complex& other) const;
+    bool operator!=(const Complex& other) const;
+    
+    // Math functions
+    double magnitude() const;
+    double phase() const;
+    Complex conjugate() const; // Сопряженное 
+    Complex reciprocal() const; //Обратное на русский...
+    
+    // Utilits
+    std::string to_string() const;
+    static Complex from_polar(double magnitude, double phase);
+};
+
+} 
