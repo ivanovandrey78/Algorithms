18e38ef92f5363d0e4eee7a3be0a4cbdac15e3ae
ivanovandrey78 <jaydenpull1@gmail.com>
2025-10-22 01:49:01 +0300

    add friends operators

--- algorithms/math/complex.h
+++ algorithms/math/complex.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 
 namespace math {
     
@@ -12,29 +13,36 @@ private:
     
 public:
     // Construct. 
-
     // Default constructor - 0 + 0i 
-    Complex(); 
-
-    Complex(double r);
-
-    Complex(double r = 0.0, double i = 0.0); // standart initialisation
-
-    Complex(const Complex &other); // copy wth other number 
+    Complex();                               // 0 + 0i
+    Complex(double r);                       // r + 0i  
+    Complex(double r, double i);             // r + i
+    Complex(const Complex &other) = default; // Copy from other number 
     
-    // Get-operators.
+    // Getters.
     double getReal() const;
     double getImag() const;
 
-    // Edit-operators 
+    // Setters.
     void setReal(double r); 
     void setImag(double i);
     
     // Arithmetics functions.
-    Complex operator+(const Complex& other) const;
-    Complex operator-(const Complex& other) const;
-    Complex operator*(const Complex& other) const;
-    Complex operator/(const Complex& other) const;
+    // Compound operators.
+    Complex operator+=(const Complex& other);
+    Complex operator-=(const Complex& other);
+    Complex operator*=(const Complex& other);
+    Complex operator/=(const Complex& other);
+
+    //Unary operators
+    Complex operator-() const;
+    Complex operator+() const;
+
+    //Binary operators
+    friend Complex operator+(const Complex& lhs,const Complex& rhs);
+    friend Complex operator-(const Complex& lhs,const Complex& rhs);
+    friend Complex operator*(const Complex& lhs,const Complex& rhs);
+    friend Complex operator/(const Complex& lhs,const Complex& rhs);
     
     // Compare.
     bool operator==(const Complex& other) const;
@@ -44,11 +52,15 @@ public:
     double magnitude() const;
     double phase() const;
     Complex conjugate() const; // Сопряженное 
-    Complex reciprocal() const; //Обратное на русский...
+    Complex reciprocal() const; 
+
+    // Stream Complex number
+    friend std::ostream& operator<<(std::ostream& os, const Complex& c);
+    friend std::istream& operator>>(std::istream& is, Complex& c);
     
     // Utilits.
     std::string to_string() const;
     static Complex from_polar(double magnitude, double phase);
-};
+}; 
 
-} 
+} // namespace math
